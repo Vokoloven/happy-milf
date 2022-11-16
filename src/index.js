725d44d9608fea0220c40df4a7c4b188a7a4320b
OlesRezvaniuk <orezvaniuk@gmail.com>
2022-11-16 14:24:02 +0200

    к
    к

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/react-homework-template">
+    <BrowserRouter basename="/happy-milf">
       <App />
     </BrowserRouter>
   </React.StrictMode>
