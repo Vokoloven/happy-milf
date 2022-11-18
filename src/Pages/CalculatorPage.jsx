6b5e7b21a223d00851d69173a60f51afce6ee551
DanilStan <stanislavov.danik@gmail.com>
2022-11-19 00:08:49 +0200

    Summary

--- src/Pages/CalculatorPage.jsx
+++ src/Pages/CalculatorPage.jsx
@@ -1,40 +1,13 @@
-import { useState } from 'react';
+import { CalendarForm } from 'components/CalendarForm';
+import { CalendarSection } from 'components/CalendarSection';
+
+import 'react-calendar/dist/Calendar.css';
 
 export const CalculatorPage = () => {
-  const [productName, setProductName] = useState('');
-  const [grams, setGrams] = useState('');
-
-  const handleProductName = e => {
-    setProductName(e.currentTarget.value);
-  };
-
-  const handleGrams = e => {
-    setGrams(e.currentTarget.value);
-  };
-
-  const handleCalculationSubmit = e => {
-    e.preventDefault();
-    console.log(productName, grams);
-  };
-
   return (
     <section style={{ paddingTop: '30px' }}>
-      <form onSubmit={handleCalculationSubmit}>
-        <label>
-          Enter product name
-          <input value={productName} onChange={handleProductName} type="text" />
-        </label>
-        <label>
-          Grams
-          <input value={grams} onChange={handleGrams} min="100" type="number" />
-        </label>
-        <button>+</button>
-      </form>
-      <ul>
-        <li>product</li>
-        <li>product</li>
-        <li>product</li>
-      </ul>
+      <CalendarSection />
+      <CalendarForm />
     </section>
   );
 };
