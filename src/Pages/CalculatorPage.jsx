fa4daeb6c17105e65025bedec85677805ea55be3
OlesRezvaniuk <orezvaniuk@gmail.com>
2022-11-18 22:22:30 +0200

    к
    к

--- src/Pages/CalculatorPage.jsx
+++ src/Pages/CalculatorPage.jsx
@@ -1,3 +1,40 @@
+import { useState } from 'react';
+
 export const CalculatorPage = () => {
-  return <>Сторінка калькулятор</>;
+  const [productName, setProductName] = useState('');
+  const [grams, setGrams] = useState('');
+
+  const handleProductName = e => {
+    setProductName(e.currentTarget.value);
+  };
+
+  const handleGrams = e => {
+    setGrams(e.currentTarget.value);
+  };
+
+  const handleCalculationSubmit = e => {
+    e.preventDefault();
+    console.log(productName, grams);
+  };
+
+  return (
+    <section style={{ paddingTop: '30px' }}>
+      <form onSubmit={handleCalculationSubmit}>
+        <label>
+          Enter product name
+          <input value={productName} onChange={handleProductName} type="text" />
+        </label>
+        <label>
+          Grams
+          <input value={grams} onChange={handleGrams} min="100" type="number" />
+        </label>
+        <button>+</button>
+      </form>
+      <ul>
+        <li>product</li>
+        <li>product</li>
+        <li>product</li>
+      </ul>
+    </section>
+  );
 };
